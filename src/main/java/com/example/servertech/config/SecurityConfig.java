66fd71ad317e65d6cfa7827a9a24fb1d3a135221
LeeShinHeang <dragon11796@gmail.com>
2025-04-01 14:52:33 +0900

    feat: Spring Security 설정

---
+++ src/main/java/com/example/servertech/config/SecurityConfig.java
@@ -0,0 +1,103 @@
+package com.example.servertech.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+			.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+			.csrf(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(SWAGGER_PATTERNS).permitAll()
+				.requestMatchers(STATIC_RESOURCES_PATTERNS).permitAll()
+				.requestMatchers(PERMIT_ALL_PATTERNS).permitAll()
+				.requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+				.requestMatchers(AUTH_ENDPOINTS).permitAll()
+				.anyRequest().permitAll()
+			)
+			.build();
+	}
+
+	private static final String[] SWAGGER_PATTERNS = {
+		"/swagger-ui/**",
+		"/actuator/**",
+		"/v3/api-docs/**",
+	};
+
+	private static final String[] STATIC_RESOURCES_PATTERNS = {
+		"/img/**",
+		"/css/**",
+		"/js/**",
+		"/cloud/**",
+	};
+
+	private static final String[] PERMIT_ALL_PATTERNS = {
+		"/error",
+		"/favicon.ico",
+		"/index.html",
+		"/",
+	};
+
+	private static final String[] PUBLIC_ENDPOINTS = {
+	};
+
+	private static final String[] AUTH_ENDPOINTS = {
+		"/api/auth/login",
+		"/api/auth/register",
+	};
+
+	CorsConfigurationSource corsConfigurationSource() {
+		return request -> {
+			CorsConfiguration config = new CorsConfiguration();
+			config.setAllowedHeaders(Collections.singletonList("*"));
+			config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+			config.setAllowedOriginPatterns(
+				Arrays.asList(
+					"http://localhost:3000",
+					"http://localhost:8080"
+				)
+			);
+			config.setAllowCredentials(true);
+			return config;
+		};
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public RoleHierarchy roleHierarchy() {
+		return RoleHierarchyImpl.withDefaultRolePrefix()
+			.role("ADMIN").implies("NORMAL")
+			.build();
+	}
+}
