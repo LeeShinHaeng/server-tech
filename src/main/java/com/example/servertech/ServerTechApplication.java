ef4dc93b00bb3abd7fa8d6ab40c6989d5ae354dc
LeeShinHeang <dragon11796@gmail.com>
2025-04-01 09:47:11 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/example/servertech/ServerTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.servertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servertech/ServerTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servertech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
