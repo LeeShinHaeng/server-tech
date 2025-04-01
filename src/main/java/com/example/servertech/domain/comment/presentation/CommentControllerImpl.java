1ae183a429fd192969f5d590d395cbc23fcc8349
LeeShinHeang <dragon11796@gmail.com>
2025-04-01 14:43:02 +0900

    feat: 엔드포인트 설정

--- src/main/java/com/example/servertech/domain/comment/presentation/CommentControllerImpl.java
+++ src/main/java/com/example/servertech/domain/comment/presentation/CommentControllerImpl.java
@@ -2,10 +2,12 @@ package com.example.servertech.domain.comment.presentation;
 
 import com.example.servertech.domain.comment.application.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/comments")
 public class CommentControllerImpl implements CommentController {
 	private final CommentService commentService;
 }
--- src/main/java/com/example/servertech/domain/post/presentation/PostControllerImpl.java
+++ src/main/java/com/example/servertech/domain/post/presentation/PostControllerImpl.java
@@ -2,10 +2,12 @@ package com.example.servertech.domain.post.presentation;
 
 import com.example.servertech.domain.post.application.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/posts")
 public class PostControllerImpl implements PostController {
 	private final PostService postService;
 }
