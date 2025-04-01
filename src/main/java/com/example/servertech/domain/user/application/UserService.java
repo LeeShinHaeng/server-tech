7f4ee667670145461db8d9f453d773fa37a99c0c
LeeShinHeang <dragon11796@gmail.com>
2025-04-01 15:18:20 +0900

    feat: 시험용 엔드포인트 생성

--- src/main/java/com/example/servertech/domain/user/application/UserService.java
+++ src/main/java/com/example/servertech/domain/user/application/UserService.java
@@ -5,15 +5,19 @@ import com.example.servertech.domain.user.presentation.request.CreateUserRequest
 import com.example.servertech.domain.user.presentation.response.UserPersistResponse;
 import com.example.servertech.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
 
 	public UserPersistResponse register(CreateUserRequest request) {
-		User user = User.createNormal(request.name(), request.email(), request.password());
+		String encode = passwordEncoder.encode(request.password());
+		User user = User.createNormal(request.name(), request.email(), encode);
 		User save = userRepository.save(user);
 		return UserPersistResponse.of(save.getId());
 	}
