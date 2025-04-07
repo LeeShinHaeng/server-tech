7b63d6e0e0357f596351651f12b034d49045e80d
LeeShinHeang <dragon11796@gmail.com>
2025-04-07 19:49:17 +0900

    refactor: 테스트 시 key 주입을 위해 JwtProperties 사용

--- src/main/java/com/example/servertech/auth/jwt/JwtProvider.java
+++ src/main/java/com/example/servertech/auth/jwt/JwtProvider.java
@@ -27,11 +27,7 @@ import static io.jsonwebtoken.SignatureAlgorithm.HS256;
 public class JwtProvider {
 	private final static String HEADER_AUTHORIZATION = "Authorization";
 	private final static String TOKEN_PREFIX = "Bearer ";
-	@Value("${jwt.issuer}")
-	private String JWT_ISSUER;
-	@Value("${jwt.key}")
-	private String SECRET_KEY;
-
+	private final JwtProperties jwtProperties;
 
 	public String generateAccessToken(Long id, UserRole role) {
 		Date now = new Date();
@@ -43,12 +39,12 @@ public class JwtProvider {
 
 		return Jwts.builder()
 			.setHeaderParam(TYPE, JWT_TYPE)
-			.setIssuer(JWT_ISSUER)
+			.setIssuer(jwtProperties.getIssuer())
 			.setIssuedAt(now)
 			.setExpiration(expiry)
 			.setSubject(id.toString())
 			.claim("role", role.name())
-			.signWith(HS256, SECRET_KEY)
+			.signWith(HS256, jwtProperties.getKey())
 			.compact();
 	}
 
@@ -59,7 +55,7 @@ public class JwtProvider {
 
 		try {
 			Jwts.parser()
-				.setSigningKey(SECRET_KEY)
+				.setSigningKey(jwtProperties.getKey())
 				.parseClaimsJws(token);
 			return true;
 		} catch (Exception e) {
@@ -100,7 +96,7 @@ public class JwtProvider {
 
 	private Claims getClaims(String token) {
 		return Jwts.parser()
-			.setSigningKey(SECRET_KEY)
+			.setSigningKey(jwtProperties.getKey())
 			.parseClaimsJws(token)
 			.getBody();
 	}
